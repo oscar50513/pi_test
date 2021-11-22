786af9397108c37af0973f742c1bb0145ee978bb
oscar50513 <74083913+oscar50513@users.noreply.github.com>
2021-11-22 18:46:16 +0800

    11/22

--- camera_pi.py
+++ camera_pi.py
@@ -10,17 +10,16 @@ class Camera(BaseCamera):
         with picamera.PiCamera() as camera:
             # let camera warm up
             time.sleep(2)
-            int i = 0
+            
             stream = io.BytesIO()
             for _ in camera.capture_continuous(stream, 'jpeg',
                                                  use_video_port=True):
                 # return current frame
                 stream.seek(0)
                 yield stream.read()
-                with open("test\\" + str(i) + "jpeg", "wb") as file:
+                with open("test\\" + str(_) + "jpeg", "wb") as file:
                     file.write(stream)
                 time.sleep(2)
-                i+=1
                 # reset stream for next frame
                 stream.seek(0)
                 stream.truncate()
